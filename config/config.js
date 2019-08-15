b9f6250a86284bf41234f5ba82ab275f915d1838
Kate Mullan <katemullan@AND0763.local>
2019-08-15 09:34:46 +0100

    naming

--- config/config.js
+++ config/config.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 db; 'mongodb://kmullan:test123@localhost:27017/auth-login',
 db_dev; 'mongodb://kmullan:test123@localhost:27017/auth-login'
-=======
-db: 'mongodb://kmullan:test123@localhost:27017/auth-login',
-db_dev: 'mongodb://kmullan:test123@localhost:27017/auth-login'
->>>>>>> 93b22a194fb4df3ef1f6475f8d117180cc342de6
--- server.js
+++ server.js
@@ -30,5 +30,5 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
-const port = process.env.PORT || 5000 || 27017; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
